3bfd333340e7b99399d7f0a4a4b251b89be15a59
alexandersokolow <alexandersokolow96@gmail.com>
2024-01-25 21:43:15 +0100

    fix and upgrade

--- index.js
+++ index.js
@@ -1,115 +1,103 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/";
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://0.0.0.0:27017/";
 
 function makeFunctions(database) {
-
   const doFind = async (collection, query) => {
-    return new Promise((resolve, reject) => {
-      MongoClient.connect(url, (err, db) => {
-        if (err) throw err;
-        var dbo = db.db(database);
-        dbo.collection(collection).find(query).toArray((err, result) => {
-          db.close();
-          if (err) reject(err);
-          resolve(result);
-        });
-      });
-    });
-  }
+    const client = new MongoClient(uri);
+    try {
+      console.log("trying to connect...");
+      await client.connect();
+      console.log("connected!");
+      var dbo = client.db(database);
+      const result = await dbo.collection(collection).find(query).toArray();
+      return result;
+    } catch (err) {
+      console.log("err: ", err);
+    } finally {
+      client.close();
+    }
+  };
 
   const doFindOne = async (collection, query) => {
-    return new Promise((resolve, reject) => {
-      MongoClient.connect(url, (err, db) => {
-        if (err) throw err;
-        var dbo = db.db(database);
-        dbo.collection(collection).findOne(query, (err, result) => {
-          db.close();
-          if (err) reject(err);
-          resolve(result);
-        });
-      });
-    });
-  }
-
-  const doFindSort = async (collection, query, sort) => {
-    return new Promise((resolve, reject) => {
-      MongoClient.connect(url, (err, db) => {
-        if (err) throw err;
-        var dbo = db.db(database);
-        dbo.collection(collection).find(query, sort).toArray((err, result) => {
-          db.close();
-          if (err) reject(err);
-          resolve(result);
-        });
-      });
-    });
-  }
+    const client = new MongoClient(uri);
+    try {
+      await client.connect();
+      var dbo = client.db(database);
+      const result = await dbo.collection(collection).findOne(query);
+      return result;
+    } catch (err) {
+      console.log("err: ", err);
+    } finally {
+      client.close();
+    }
+  };
 
   const doUpdate = async (collection, query, setter) => {
-    return new Promise((resolve, reject) => {
-      MongoClient.connect(url, (err, db) => {
-        if (err) throw err;
-        var dbo = db.db(database);
-        dbo.collection(collection).updateOne(query, setter, { multi: true }, (err, result) => {
-          db.close();
-          if (err) reject(err)
-          resolve(result);
-        });
-      });
-    });
-  }
+    const client = new MongoClient(uri);
+    try {
+      await client.connect();
+      var dbo = client.db(database);
+      const result = await dbo
+        .collection(collection)
+        .updateOne(query, setter, { multi: true });
+      return result;
+    } catch (err) {
+      console.log("err: ", err);
+    } finally {
+      client.close();
+    }
+  };
 
   const doUpdateOne = async (collection, query, setter) => {
-    return new Promise((resolve, reject) => {
-      MongoClient.connect(url, (err, db) => {
-        if (err) throw err;
-        var dbo = db.db(database);
-        dbo.collection(collection).updateOne(query, setter, (err, result) => {
-          db.close();
-          if (err) reject(err)
-          resolve(result);
-        });
-      });
-    });
-  }
+    const client = new MongoClient(uri);
+    try {
+      await client.connect();
+      var dbo = client.db(database);
+      const result = await dbo.collection(collection).updateOne(query, setter);
+      return result;
+    } catch (err) {
+      console.log("err: ", err);
+    } finally {
+      client.close();
+    }
+  };
 
   const doInsert = async (collection, values) => {
-    return new Promise((resolve,reject) => {
-      MongoClient.connect(url, (err, db) => {
-        if (err) reject(err);
-        var dbo = db.db(database);
-        dbo.collection(collection).insertOne(values, (err, result) => {
-          db.close();
-          if (err) reject(err);
-          resolve(result);
-        });
-      });
-    });
-  }
+    const client = new MongoClient(uri);
+    try {
+      await client.connect();
+      var dbo = client.db(database);
+      const result = await dbo.collection(collection).insertOne(values);
+      return result;
+    } catch (err) {
+      console.log("err: ", err);
+    } finally {
+      client.close();
+    }
+  };
 
   const doDelete = async (collection, query) => {
-    return new Promise((resolve, reject) => {
-      MongoClient.connect(url, (err, db) => {
-        if (err) throw err;
-        var dbo = db.db(database);
-        dbo.collection(collection).deleteMany(query, (err, result) => {
-          db.close();
-          if (err) reject(err)
-          resolve(result);
-        });
-      });
-    });
-  }
+    const client = new MongoClient(uri);
+    try {
+      await client.connect();
+      var dbo = client.db(database);
+      const result = await dbo.collection(collection).deleteMany(query);
+      return result;
+    } catch (err) {
+      console.log("err: ", err);
+    } finally {
+      client.close();
+    }
+  };
 
   return {
     doFind,
-    doFindSort,
     doFindOne,
     doUpdate,
     doUpdateOne,
     doInsert,
-    doDelete
-  }
+    doDelete,
+  };
 }
 
 module.exports = makeFunctions;
